9e936b539632ef77e5fd16e694ac8efb5540082a
Johnsavadkuhi <physitime@gmail.com>
2020-10-01 20:09:12 +0330

    update

--- index.js
+++ index.js
@@ -1,17 +1,4 @@
 
-function replaceSpaces(str ){
-    str = str.trim()
-    const strArray = str.split('') ; 
-    console.log(strArray) ; 
+function split(sp){
     
-    for(let i = 0 ; i < strArray.length ; i++ ){
-        if(strArray[i] === ' '){
-            strArray[i] = '%20' ; 
-        }
-        
-    }
-
-    console.log(strArray.join(''));
 }
-
-replaceSpaces("h i    ")
