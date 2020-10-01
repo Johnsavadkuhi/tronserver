deefb705a90ab9305c0c92cb394b608d1271202a
Johnsavadkuhi <physitime@gmail.com>
2020-10-01 20:10:31 +0330

    update

--- index.js
+++ index.js
@@ -1,4 +1,12 @@
 
-function split(sp){
-    
+function split(str){
+let arr = []; 
+
+for(let i = 0 ; i< str.length  ; i++ ){
+    arr.push(str[i]);
 }
+
+return arr ; 
+}
+
+console.log(split("hello"));
