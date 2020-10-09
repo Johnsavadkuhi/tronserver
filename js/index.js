cf770901f2290b2ba468a2b1127d44827e16f65a
Johnsavadkuhi <physitime@gmail.com>
2020-10-09 23:38:49 +0330

    update

--- js/index.js
+++ js/index.js
@@ -1,17 +1,19 @@
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+// var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
 
-var data = "id=5186431";
+// var data = "id=5186431";
 
-var xhr = new XMLHttpRequest();
-xhr.withCredentials = true;
+// var xhr = new XMLHttpRequest();
+// xhr.withCredentials = true;
 
-xhr.addEventListener("readystatechange", function() {
-  if(this.readyState === 4) {
-    console.log(this.responseText);
-  }
-});
 
-xhr.open("POST", "https://olu.ninisite.com/Home/GetOnlineUsers");
-xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+//   xhr.addEventListener("readystatechange", function() {
+//     if(this.readyState === 4) {
+//       console.log(this.responseText);
+//     }
+//   });
+  
+//   xhr.open("POST", "https://olu.ninisite.com/Home/GetOnlineUsers");
+//   xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
+  
+//   xhr.send(data);
 
-xhr.send(data);
