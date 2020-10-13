595eaca0c9b0b03a0734e1e29db51433e8a7473f
johnsavadkuhi <physitime@gmail.com>
2020-10-13 14:40:12 +0330

    update

--- python/main.py
+++ python/main.py
@@ -1,4 +1,10 @@
 
+import os  
 
-thiset = {"hello" , "hi", "mohammad" , "hi"}
+en = os.listdir(".")
 
+print("list dir : " , en[0] )
+
+with open("/home/mmc/tronserver/python/sqli.txt") as f: 
+    data = f.read() 
+    print(data)
