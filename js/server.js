a5a77e8a5cf929bc9ead878479d647f89bdcddbd
johnsavadkuhi <physitime@gmail.com>
2020-10-15 02:16:16 +0330

    update

--- js/server.js
+++ js/server.js
@@ -1,11 +1,3 @@
 
 const fs  = require("fs") 
 
-fs.rename('/home/mmc/tronserver/js/x' , '/home/mmc/tronserver/js/xz' , (err) =>{
-  if(err ) throw err 
-
-  fs.stat('/home/mmc/tronserver/js/xz' , (err , stats )=> {
-    if (err ) throw err 
-    console.log(`stats : ${JSON.stringify(stats)}`);
-  })
-})  
