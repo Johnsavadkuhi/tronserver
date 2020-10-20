cd87d2a61b3227eb312e45915bb51835a9e726a7
Johnsavadkuhi <physitime@gmail.com>
2020-10-20 22:21:03 +0330

    update

--- python/main.py
+++ python/main.py
@@ -1,4 +1,17 @@
 
 
-thiset = {"hello" , "hi", "mohammad" , "hi"}
+x = 5 
+print(x , type(x)) 
 
+
+y  = "programming" ;
+print(y , type(y)) ;
+
+w = 45555.0 
+print(w , type(w)) 
+
+b = True 
+bb = False 
+
+
+print(b , bb  , type(b) , type(bb))
