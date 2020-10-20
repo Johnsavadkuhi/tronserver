57d6175ccdaabc209134985037a30e561d1adb23
Johnsavadkuhi <physitime@gmail.com>
2020-10-20 22:21:35 +0330

    update

--- js/server.js
+++ js/server.js
@@ -1,11 +1,8 @@
-const express = require('express')
-const app = express()
-const port = 3000
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+let x = 3  
+let y = 5 
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+console.log(x + y ) ;
+console.log(x - y  );
+
+console.log( 3 ** 5 );
